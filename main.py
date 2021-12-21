94157f6578289698a8eee5e45fde9f7ea877eefe
bbarnabe <96452785+bbarnabe@users.noreply.github.com>
2021-12-20 20:19:39 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+# This is a Geography Quiz Game
+
+print('Hello, what is your name?')
+myname = input('')
+
+print('Okie dokie ' + myname)
+print('We are going to take a Geography quiz. Get ready')
+
+print('What is the capital of Nebraska?')
+
+Answer = input('')
+if Answer == "Lincoln":
+    print("Congrats! You got it!")
+
+
+
